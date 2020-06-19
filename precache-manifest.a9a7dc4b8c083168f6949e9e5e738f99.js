b6ea21b02b0ef1bbd8513c5cbd828e37c94d859f
Carolina Gonzalez <carolinagonzalez@MacBook-Pro-de-Carolina.local>
2020-06-18 21:36:18 -0300

    Updates

---
+++ precache-manifest.a9a7dc4b8c083168f6949e9e5e738f99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053751aa63d751bc41d4e9bc4758244f",
+    "url": "/monsters-react-practice/index.html"
+  },
+  {
+    "revision": "9117702157b9311d4bfc",
+    "url": "/monsters-react-practice/static/css/main.d36f6675.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-react-practice/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react-practice/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9117702157b9311d4bfc",
+    "url": "/monsters-react-practice/static/js/main.75931607.chunk.js"
+  },
+  {
+    "revision": "8c9937aaa8a48822ee30",
+    "url": "/monsters-react-practice/static/js/runtime-main.799e7276.js"
+  }
+]);
